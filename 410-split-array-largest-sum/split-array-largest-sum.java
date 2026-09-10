a54a9f481afd692933d12365f694862c3d8f6b91
Rakshana R <rakshanaravi527@gmail.com>
2026-09-10 22:13:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (92.32%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int splitArray( int[ ] nums , int  k )  {
+        int low = 0 , high = 0 ;
+        for( int  i : nums ){
+            high += i ;
+            low = Math.max( i , low ) ;
+        }
+        return find( low , high , nums , k ) ;
+    }
+    private int find( int l , int h , int n [ ] , int k ) {
+        if( l >h ) return l;
+        int c = 1 , m = l + ( h - l ) / 2 , sum = 0 ;
+        for(int i : n ) {
+            if( i + sum > m ){
+                sum = i ;
+                c ++ ;
+            }
+            else    sum += i ;
+        }
+        if(c <= k ) return find( l , m - 1 , n , k ) ;
+        return find( m + 1 , h , n , k ) ;
+    }
+}
