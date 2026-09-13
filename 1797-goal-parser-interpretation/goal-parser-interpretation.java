92b54d5136d452909f191c4d92f55fe7f9131405
Rakshana R <rakshanaravi527@gmail.com>
2026-09-13 23:43:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (19.21%) - LeetSync

---
+++ 1797-goal-parser-interpretation/goal-parser-interpretation.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < command.length(); i++) {
+            if (command.charAt(i) == 'G') {
+                result.append("G");
+            } else if (command.charAt(i) == '(' && command.charAt(i + 1) == ')') {
+                result.append("o");
+                i++;
+            } else {
+                result.append("al");
+                i += 3;
+            }
+        }
+
+        return result.toString();
+    }
+}
