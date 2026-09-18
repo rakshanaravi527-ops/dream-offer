0380498f645e5dea76fba55154d950fd048daa19
Rakshana R <rakshanaravi527@gmail.com>
2026-09-18 21:41:48 +0530

    Time: 8 ms (68.08%) | Memory: 47.8 MB (57.56%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,31 @@
+class Solution {
+        public int minEatingSpeed(int[] piles, int h) {
+                int left = 1;
+                        int right = 0;
+
+                                // Find the maximum pile
+                                        for (int pile : piles) {
+                                                    right = Math.max(right, pile);
+                                                            }
+
+                                                                    while (left < right) {
+                                                                                int mid = left + (right - left) / 2;
+
+                                                                                            long hours = 0;
+
+                                                                                                        for (int pile : piles) {
+                                                                                                                        hours += (pile + mid - 1) / mid;
+                                                                                                                                    }
+
+                                                                                                                                                if (hours <= h) {
+                                                                                                                                                                // Koko can finish, so try a smaller speed
+                                                                                                                                                                                right = mid;
+                                                                                                                                                                                            } else {
+                                                                                                                                                                                                            // Too slow, increase the speed
+                                                                                                                                                                                                                            left = mid + 1;
+                                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                                                }
+
+                                                                                                                                                                                                                                                        return left;
+                                                                                                                                                                                                                                                            }
+                                                                                                                                                                                                                                                            }
