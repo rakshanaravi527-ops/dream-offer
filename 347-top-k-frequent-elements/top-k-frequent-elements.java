14e15c0647d10d587758be1c3080db59902fd497
Rakshana R <rakshanaravi527@gmail.com>
2026-09-24 22:02:22 +0530

    Time: 16 ms (39.45%) | Memory: 47.7 MB (49.23%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+
+            // Step 1: Count frequency
+                    HashMap<Integer, Integer> map = new HashMap<>();
+
+                            for (int num : nums) {
+                                        map.put(num, map.getOrDefault(num, 0) + 1);
+                                                }
+
+                                                        // Step 2: Min Heap based on frequency
+                                                                PriorityQueue<Integer> pq = new PriorityQueue<>(
+                                                                            (a, b) -> map.get(a) - map.get(b)
+                                                                                    );
+
+                                                                                            // Step 3: Keep only k elements
+                                                                                                    for (int num : map.keySet()) {
+                                                                                                                pq.offer(num);
+
+                                                                                                                            if (pq.size() > k) {
+                                                                                                                                            pq.poll();
+                                                                                                                                                        }
+                                                                                                                                                                }
+
+                                                                                                                                                                        // Step 4: Create answer
+                                                                                                                                                                                int[] result = new int[k];
+
+                                                                                                                                                                                        for (int i = k - 1; i >= 0; i--) {
+                                                                                                                                                                                                    result[i] = pq.poll();
+                                                                                                                                                                                                            }
+
+                                                                                                                                                                                                                    return result;
+                                                                                                                                                                                                                        }
+                                                                                                                                                                                                                        }
